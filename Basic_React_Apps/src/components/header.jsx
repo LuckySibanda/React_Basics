c548f1d02e6790cba0e54bfea94bf5d6c0848263
Kuyl.dev <sibandalucky02@gmail.com>
2023-10-16 18:59:50 +0200

    added product webpage visual assets

---
+++ Basic_React_Apps/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="img--logo-container">
+                <img src="" alt="" className="img--logo" />
+            </div>
+
+            <div className="header--info">
+                <div>Pricing</div>
+                <div>Contact</div>
+                <div>About</div>
+            </div>
+        </header>
+    )
+}
