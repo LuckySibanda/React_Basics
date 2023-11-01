349c7ce0bb012199d3f3c0e922d519cb08af95b6
Kuyl.dev <sibandalucky02@gmail.com>
2023-11-01 03:04:21 +0200

    added a form for the customer to inititate service with owner

---
+++ Basic_portfolio_site_with_react/src/From.jsx
@@ -0,0 +1,37 @@
+export default function Form() {
+    return(
+        <>
+        <h2>Contact Us</h2>
+        <form action="">
+            <div>
+                <label htmlFor="">
+                    Name
+                </label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label htmlFor="">
+                    Name
+                </label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label htmlFor="">
+                    Name
+                </label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label htmlFor="">
+                    Name
+                </label>
+                <input type="text" />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        </>
+    )
+}
