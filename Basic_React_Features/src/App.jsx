cb0189b9d587fca2ccdd5dd01d161944c901ff90
Kuyl.dev <sibandalucky02@gmail.com>
2023-11-03 23:01:53 +0200

    doing some react.useState challanges

--- Basic_React_Features/src/App.jsx
+++ Basic_React_Features/src/App.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import './App.css'
+// import './App.css'
 
 function App() {
 
-    const [x, setX] = React.useState(0)
-    let [disp, setDisp] = React.useState('')
+    // Challange: Replace=ing a hard coded value with an initialized state with React.useState()
 
-    function Add() {
-        setX(x + 1)
+    // const result = React.useState('Hello')
+
+    // becasue result returns an array with ['hell', f()] f() is a function
+
+    // const [result, func] = React.useState("yes")
+
+    // this is array destructering, the array is separeted into its parts, the value and the fucntion that sets the value
+
+    const [isImportant, setIsImportant] = React.useState("yes")
+
+    function handleClick() {
+        setIsImportant("No")
+        // React.useState provides a function that when called gives us the ability to change state or the value in the state, so here we're the value from "yes" to "no"
     }
-
-    function Sub() {
-        setX(x - 1)
-    }
-
-    function Alert(){
-        setDisp(()=> {
-            disp = 'this is a display';
-
-            alert(disp)
-        })
-    }
-
 
     return (
-    <>
-        <button onClick={Add}>+</button>
-        <div>{x}</div>
-        <button onClick={Sub}>-</button>
-
-        <button onClick={Alert}>Press Me</button>
-    </>
+        <div className="stte">
+            <h1 className="state--tit">IS this state important to know?</h1>
+            <div onClick={handleClick}>
+                <button>{isImportant}</button>
+            </div>
+        </div>
     )
 }
 
