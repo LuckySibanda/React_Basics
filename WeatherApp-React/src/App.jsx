41c5bd32d4a871ec966c8b0313190abad8586f30
Kuyl.dev <sibandalucky02@gmail.com>
2023-11-05 15:56:14 +0200

    function to call the api, display api response

--- WeatherApp-React/src/App.jsx
+++ WeatherApp-React/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const apiKey = "887e67b6c4564982a5e212846231110"
+
+    // const apiUrl = "https://api.weatherapi.com/v1/forecast.json?key=887e67b6c4564982a5e212846231110&q=Midrand&days=3&aqi=no&alerts=no"
+
+    const [weatherData, setWeatherData] = React.useState(null)
+
+    React.useEffect(() => {
+        async function getWeather() {
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Midrand&days=3&aqi=no&alerts=no`)
+
+            let data = await response.json();
+
+            setWeatherData(data)
+        }
+
+        getWeather()
+        
+    }, [apiKey]);
+
+    console.log(weatherData)
+
+    const description = weatherData.current.condition.text
+    const temp = weatherData.current.temp_c
+    const location = weatherData.location.name
+
+
+
+
+    return (
+        <>
+            
+
+                {weatherData ? (
+                    <div>
+                        <div>
+                        {/* {weatherData.current.condition.text} */}
+                        {description}
+                        </div>
+                        <div>
+                            {/* {weatherData.current.temp_c} */}
+                            {temp}
+                        </div>
+                        <div>
+                            {/* {weatherData.location.name} */}
+                            {location}
+                        </div>
+                    </div>
+                ) : (
+                    <p>nothing to  see hear</p>
+                )}
+
+
+            
+        </>
+    )
 }
 
 export default App
